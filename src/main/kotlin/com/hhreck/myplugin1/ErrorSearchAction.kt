2d13a85ec80e6fd5415796c69487a9f3be42f28d
vikaaaleks <vikaaaleks@mail.ru>
2023-11-29 18:08:37 +0300

    new changes

--- src/main/kotlin/com/hhreck/myplugin1/ErrorSearchAction.kt
+++ src/main/kotlin/com/hhreck/myplugin1/ErrorSearchAction.kt
@@ -1,4 +1,35 @@
 package com.hhreck.myplugin1
 
-class ErrorSearchAction {
+import com.intellij.openapi.actionSystem.AnAction
+import com.intellij.openapi.actionSystem.AnActionEvent
+import com.intellij.openapi.actionSystem.PlatformDataKeys
+import com.intellij.openapi.ide.CopyPasteManager
+import java.awt.datatransfer.StringSelection
+import java.net.URLEncoder
+import java.awt.Desktop
+import java.net.URI
+
+class ErrorSearchAction : AnAction() {
+    override fun actionPerformed(e: AnActionEvent) {
+
+        val editor = e.getData(PlatformDataKeys.EDITOR) ?: return
+        val document = editor.document
+        val selectedText = editor.selectionModel.selectedText
+
+        val errorText = selectedText ?: document.text
+        if (errorText.isBlank()) return
+
+        val selection = StringSelection(errorText)
+        CopyPasteManager.getInstance().setContents(selection)
+
+        try {
+            val encodedText = URLEncoder.encode(errorText, "UTF-8")
+            val uri = URI("https://stackoverflow.com/search?q=$encodedText")
+            if (Desktop.isDesktopSupported()) {
+                Desktop.getDesktop().browse(uri)
+            }
+        } catch (ex: Exception) {
+            ex.printStackTrace()
+        }
+    }
 }
